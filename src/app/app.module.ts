24b30f3bac4d744e29dfca87b7b120e5a62512f7
Pierre-Antoine <you@example.com>
2018-06-20 11:57:45 +0200

    changement package-lock +validators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule} from "@angular/router";
+import { AppComponent } from './app.component';
 
 import {ROUTES} from './app.routes';
 import { ReactiveFormsModule } from '@angular/forms';
 import {FormsModule} from "@angular/forms";
-import { AppComponent } from './app.component';
 import { FormulaireMissionsComponent } from './formulaire-missions/formulaire-missions.component';
+import  FormulaireMissionsServiceService  from './services/formulaireMissionService/formulaire-missions-service.service';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormulaireMissionsComponent
+    FormulaireMissionsComponent,
   ],
   imports: [
+    HttpClientModule,
     ReactiveFormsModule,
     BrowserModule,
     FormsModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [FormulaireMissionsServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
